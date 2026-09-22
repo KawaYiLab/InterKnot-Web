0c5d49a5f0958bfa4cc749fa4aa37bad681dc6e8
KawaYiLab <127044784+KawaYiLab@users.noreply.github.com>
2026-09-22 17:55:26 +0800

    Update PostCard.vue

--- app/components/PostCard.vue
+++ app/components/PostCard.vue
@@ -211,7 +211,7 @@ const handleOpen = (e: MouseEvent) => {
         </div>
 
         <h3 class="ik-card__title" :class="{ 'ik-card__title--read': post.isRead }">
-          <span v-if="post.category" class="ik-card__title-cat">[ {{ post.category.name }} ]</span>{{ post.title }}
+          <span v-if="post.category" class="ik-card__title-cat">[{{ post.category.name }}]</span>{{ post.title }}
         </h3>
       </div>
     </NuxtLink>
